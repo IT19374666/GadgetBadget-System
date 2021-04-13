031ba35264a85bcd255ffe7b015971a9a7466214
Sasanka5426 <sasanka5426@gmail.com>
2021-04-13 11:41:21 +0530

    Add new researcher completed

--- ResearchAndResearcherManagement/src/com/ResearcherService.java
+++ ResearchAndResearcherManagement/src/com/ResearcherService.java
@@ -14,6 +14,34 @@ import org.jsoup.*;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
 
+
+@Path("/Researcher")
 public class ResearcherService {
 
+	
+	Researcher researcherobj = new Researcher();
+	
+	@GET 
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String read() {
+		return "Hello";
+	}
+	
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertResearcher(@FormParam("name") String name,
+									@FormParam("email") String email,
+									@FormParam("address") String address,
+									@FormParam("phone") String phone,
+									@FormParam("interestArea") String interest,
+									@FormParam("type") String type)
+	{
+		String output = researcherobj.insertResearcher(name, email, address, phone, interest, type);
+		return output;
+				
+	}
 }
--- ResearchAndResearcherManagement/src/model/Researcher.java
+++ ResearchAndResearcherManagement/src/model/Researcher.java
@@ -1,5 +1,64 @@
 package model;
 import java.sql.*; 
 public class Researcher {
+	
+	private Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/researchandresearcher", "root", "");
+				
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public String insertResearcher(String name, String email, String address, String phone, String interest, String type) {
+		
+		String output = "";
+		
+		try {
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database to add a Researcher";
+			}
+			
+			//creating a prepared statement
+			String query = "INSERT INTO researcher(stakeholder_ID,name,email,address,phone_no,interest_area,researcher_type)"
+						+ " VALUES(?,?,?,?,?,?,?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//binding values
+			preparedStmt.setInt(1,0);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, email);
+			preparedStmt.setString(4, address);
+			preparedStmt.setString(5, phone);
+			preparedStmt.setString(6, interest);
+			preparedStmt.setString(7, type);
+			
+			//execute the statement
+			
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Reaseachr added successfully";
+		}
+		catch(Exception e) {
+			output = "Error while adding new researcher";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
 
 }
