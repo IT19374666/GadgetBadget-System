263c69c9e619405d852c9b69017d57b91a67d206
Sasanka5426 <sasanka5426@gmail.com>
2021-04-14 23:25:18 +0530

    Add new research completed.

--- ResearchAndResearcherManagement/src/com/ResearchService.java
+++ ResearchAndResearcherManagement/src/com/ResearchService.java
@@ -15,6 +15,34 @@ import org.jsoup.*;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
 
+@Path("/Research")
 public class ResearchService {
+	
+	Research researchObj = new Research();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertResearch(String researchData) {
+		
+		//convert the input string to a JSON object
+		JsonObject resObj = new JsonParser().parse(researchData).getAsJsonObject();
+		
+		//read the values from the JSON object
+		String res_topic = resObj.get("research_topic").getAsString();
+		String res_area = resObj.get("research_area").getAsString();
+		String status = resObj.get("status").getAsString();
+		String progress = resObj.get("progress").getAsString();
+		String res_ID = resObj.get("res_ID").getAsString();
+		
+		String output = researchObj.insertResearch(res_topic, res_area, status, progress, res_ID);
+		
+		
+		
+		
+		return output;
+	}
+	
 
 }
--- ResearchAndResearcherManagement/src/model/Research.java
+++ ResearchAndResearcherManagement/src/model/Research.java
@@ -1,5 +1,63 @@
 package model;
 import java.sql.*; 
+
 public class Research {
+	
+	private Connection connect() {
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/researchandresearcher", "root", "");
+				
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public String insertResearch(String res_topic, String res_area, String status, String progress, String res_ID) {
+		
+		String output = "";
+		
+		try {
+			Connection con = connect();
+			
+			if(con == null) {
+				return "Error while connecting to the database to add a Researcher";
+			}
+			
+			//creating a prepared statement
+			String query = "INSERT INTO research(research_ID,research_topic,research_area,status,progress,stakeholder_ID)"
+						+ " VALUES(?,?,?,?,?,?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//binding values
+			preparedStmt.setInt(1,0);
+			preparedStmt.setString(2, res_topic);
+			preparedStmt.setString(3, res_area);
+			preparedStmt.setString(4, status);
+			preparedStmt.setString(5, progress);
+			preparedStmt.setInt(6, Integer.parseInt(res_ID));
+			
+			
+			//execute the statement
+			
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Reasearch added successfully";
+		}
+		catch(Exception e) {
+			output = "Error while adding new research";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 }
